f443e947cded85020c5da0672eba67f1ee7cc9fa
Sou59 <sz59@gmail.com>
2019-03-05 09:11:57 +0100

    add node rest shop

---
+++ node-rest-shop/api/routes/products.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
+const ProductsControler = require('../controllers/products');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function(req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+        // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const upload = multer({ 
+    storage: storage, 
+    limits: {
+    fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
+
+router.get('/', ProductsControler.product_get_all);
+
+router.post('/', checkAuth, upload.single('productImage'), ProductsControler.products_create_product);
+
+router.get('/:productId', ProductsControler.products_get_product);
+
+router.patch('/:productId', checkAuth, ProductsControler.products_update_product);
+
+router.delete('/:productId', checkAuth, ProductsControler.products_delete);
+
+module.exports = router
+
+
+
+
